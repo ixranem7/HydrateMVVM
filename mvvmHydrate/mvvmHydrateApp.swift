b4d6f538f2733cd324fd38e67896e6c845f98511
raneem <ixranem7@gmail.com>
2024-10-28 23:58:05 +0300

    Initial Commit

---
+++ mvvmHydrate/mvvmHydrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvvmHydrateApp.swift
+//  mvvmHydrate
+//
+//  Created by Raneem on 25/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct mvvmHydrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mvvmHydrateTests/mvvmHydrateTests.swift
@@ -0,0 +1,17 @@
+//
+//  mvvmHydrateTests.swift
+//  mvvmHydrateTests
+//
+//  Created by Raneem on 25/04/1446 AH.
+//
+
+import Testing
+@testable import mvvmHydrate
+
+struct mvvmHydrateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
